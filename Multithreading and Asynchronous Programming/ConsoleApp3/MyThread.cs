a896f1618cec44f59a2aba6e1c8d27544bc63010
Kagamicchi <124303783+Kagamicchi@users.noreply.github.com>
2023-02-26 13:20:35 +0200

    Add files via upload

---
+++ Multithreading and Asynchronous Programming/ConsoleApp3/MyThread.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class MyThread
+    {
+        public void ThreadNumbers()
+        {
+            // information about thread
+            Console.WriteLine("{0} thread is using the method ThreadNumbers", Thread.CurrentThread.Name);
+            // output of numbers
+            Console.Write("Numbers: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + ", ");
+                // delay usage
+                Thread.Sleep(3000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
