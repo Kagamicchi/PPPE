d5f15e062432aa5472232bf080fa7dfb051a9d9b
Kagamicchi <124303783+Kagamicchi@users.noreply.github.com>
2023-04-25 20:35:20 +0300

    Add files via upload

---
+++ Reflection/ConsoleApp4/Cable.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Cable
+    {
+        public string title;
+        public double standard;
+        public double length;
+        public bool inStock;
+        private char type = 'A';
+        private int price;
+
+        public Cable() { }
+
+        public Cable(string title, double standard, char type, double length, int price, bool inStock)
+        {
+            this.title = title;
+            this.standard = standard;
+            this.type = type;
+            this.length = length;
+            this.price = price;
+            this.inStock = inStock;
+        }
+
+        private double ConvertToUS()
+        {
+            return price /= 40;
+        }
+
+        public double ShowPriceInUS()
+        {
+            return ConvertToUS();
+        }
+
+        public void DisplayCableType()
+        {
+            Console.Write("This is a cabel Type-" + type);
+            if (type == 'A')
+            {
+                Console.WriteLine(". It is placed on the side of the main unit");
+            } else if (type == 'B')
+            {
+                Console.WriteLine(". It is located on the side of the peripheral device");
+            } else if (type == 'C')
+            {
+                Console.WriteLine(". It has contacts arranged in a mirror (2x12),\nthanks to which any position of the plug for connection with the gadget");
+            } else
+            {
+                Console.WriteLine("Error! Type is not correct or does not exist!");
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("##### Cabel USB {0} #####\nTitle: {1}\tLength: {2} m\nPrice: {3} UAN", standard.ToString("0.0"), title, length, price);
+        }
+
+        public void Speed()
+        {
+            if (standard == 1.0)
+            {
+                Console.WriteLine("Speed: up to 1,5 Mbps\tSpecification: Low-Speed");
+            } else if (standard == 1.1)
+            {
+                Console.WriteLine("Speed: up to 12 Mbps\tSpecification: Full-Speed");
+            } else if (standard == 2.0)
+            {
+                Console.WriteLine("Speed: up to 480 Mbps\tSpecification: High-Speed");
+            } else if (standard == 3.0 || standard == 3.1 || standard == 3.2)
+            {
+                Console.WriteLine("Speed: up to 5 Gbps\tSpecification: Super-Speed");
+            } else
+            {
+                Console.WriteLine("Error! This standard is not correct or does not exist.");
+            }
+        }
+    }
+}
---
+++ Reflection/ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cable cable1 = new Cable("Hoco", 1.1, 'A', 3, 435, false);
+            cable1.Show();
+            cable1.DisplayCableType();
+            cable1.Speed();
+
+            Console.Write("\n");
+
+            Cable cable2 = new Cable("Cablexpert", 2.0, 'A', 4.5, 135, true);
+            cable2.Show();
+            Console.WriteLine("In stock: " + cable2.inStock + "\n");
+
+            Cable cable3 = new Cable("Hoco", 3.0, 'C', 2, 268, false);
+            cable3.Show();
+            cable3.DisplayCableType();
+            cable3.Speed();
+            Console.WriteLine("Converted in US: $" + cable3.ShowPriceInUS());
+            Console.WriteLine("In stock: " + cable1.inStock);
+
+            Console.WriteLine("\n###################################\n");
+
+            Type myType = typeof(Cable);
+
+            foreach (MemberInfo member in myType.GetMembers(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                Console.WriteLine($"{member.DeclaringType} {member.MemberType} {member.Name}");
+            }
+
+            Console.WriteLine("\n###################################\n");
+
+            TypeInfo typeInfo = typeof(Cable).GetTypeInfo();
+            IEnumerable<FieldInfo> fieldsList = typeInfo.DeclaredFields;
+            IEnumerable<MethodInfo> methodsList = typeInfo.DeclaredMethods;
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("### Fields: ###");
+            foreach (FieldInfo fieldInfo in fieldsList)
+            {
+
+                sb.Append("\n" + fieldInfo.DeclaringType.Name + ": " + fieldInfo.Name);
+            }
+            sb.Append("\n### Methods: ###");
+            foreach (MethodInfo methodsInfo in methodsList)
+            {
+                sb.Append("\n" + methodsInfo.DeclaringType.Name + ": " + methodsInfo.Name);
+            }
+
+            Console.WriteLine(sb.ToString());
+
+            Console.WriteLine($"\n### Information about {cable2.title} USB {cable2.standard.ToString("0.0")} cable: ###\n");
+
+            MethodInfo methodInfo = typeInfo.GetMethod("Speed");
+            var result = methodInfo.Invoke(cable2, null);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
